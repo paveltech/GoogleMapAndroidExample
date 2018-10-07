e814aef7dab77ef53469039c0b55f61b5542cd22
Ariful Islam <ariful.pust@gmail.com>
2018-10-07 17:02:55 +0600

    Initial commit

---
+++ app/src/main/java/com/example/dream71/googlemapandroidexample/GoogleLocationShow.java
@@ -0,0 +1,130 @@
+package com.example.dream71.googlemapandroidexample;
+
+
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.location.Location;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+import android.widget.Toast;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.CircleOptions;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+
+import static android.content.pm.PackageManager.PERMISSION_GRANTED;
+public class GoogleLocationShow extends FragmentActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+    private GoogleMap mMap;
+    private GoogleApiClient googleApiClient;
+    private static final int LOCATION_REQUEST_CODE = 101;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(LocationServices.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+    }
+    public void onStart() {
+        super.onStart();
+        googleApiClient.connect();
+    }
+    public void onStop() {
+        super.onStop();
+        googleApiClient.disconnect();
+    }
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        checkLocationandAddToMap();
+    }
+    @Override
+    public void onConnected(Bundle bundle) {
+        SupportMapFragment mapFragment = (SupportMapFragment)
+                getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+    @Override
+    public void onConnectionSuspended(int i) {
+    }
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+    }
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case LOCATION_REQUEST_CODE:
+                if (grantResults.length > 0 && grantResults[0] == PERMISSION_GRANTED) {
+                    checkLocationandAddToMap();
+                } else
+                    Toast.makeText(this, "Location Permission Denied", Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
+    private void checkLocationandAddToMap() {
+        //Checking if the user has granted the permission
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            //Requesting the Location permission
+            ActivityCompat.requestPermissions(this, new String[] {
+                    android.Manifest.permission.ACCESS_FINE_LOCATION
+            }, LOCATION_REQUEST_CODE);
+            return;
+        }
+
+        Location location = LocationServices.FusedLocationApi.getLastLocation(googleApiClient);
+        /*
+        //Fetching the last known location using the Fus
+
+        //MarkerOptions are used to create a new Marker.You can specify location, title etc with MarkerOptions
+        MarkerOptions markerOptions = new MarkerOptions().position(new LatLng(location.getLatitude(), location.getLongitude())).title("You are Here");
+        //Adding the created the marker on the map
+        mMap.addMarker(markerOptions);
+        */
+        CircleOptions circleOptions = new CircleOptions();
+        circleOptions.center(new LatLng(location.getLatitude(),location.getLongitude()));
+        circleOptions.radius(700);
+        circleOptions.strokeColor(Color.RED);
+        circleOptions.fillColor(0x30ff0000);
+        circleOptions.strokeWidth(6);
+
+
+        CameraUpdate center = CameraUpdateFactory.newLatLng(new LatLng(location.getLatitude(), location.getLongitude()));
+        CameraUpdate zoom = CameraUpdateFactory.zoomTo(13);
+        mMap.clear();
+
+        MarkerOptions mp = new MarkerOptions();
+        mp.position(new LatLng(location.getLatitude(), location.getLongitude()));
+        mp.title("my position");
+        mMap.addMarker(mp);
+        mMap.moveCamera(center);
+        mMap.animateCamera(zoom);
+        mMap.addCircle(circleOptions);
+
+
+        // zoom out location
+        //map.animateCamera(CameraUpdateFactory.zoomTo(map.getCameraPosition().zoom - 0.5f));
+
+
+    }
+}
